587dc91be317e763b7c18ec573f39fd11dbb9a99
Mahajan-Sachin <152908416+Mahajan-Sachin@users.noreply.github.com>
2024-04-05 09:38:30 +0530

    07_array_insertion.c

---
+++ 16 arrays/array_insertion.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+#define MAX_SIZE 100
+
+void addElementAtIndex(int array[], int size, int index, int element) {
+    if (size >= MAX_SIZE) {
+        printf("Array is full. Cannot add more elements.\n");
+        return;
+    }
+    
+    if (index < 0 || index > size) {
+        printf("Invalid index.\n");
+        return;
+    }
+    
+    // Shift elements to the right starting from the end
+    for (int i = size; i > index; i--) {
+        array[i] = array[i - 1];
+    }
+    
+    // Insert the new element at the specified index
+    array[index] = element;
+    size++;
+    
+    // Print the updated array
+    printf("Array after adding element:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[MAX_SIZE];
+    int size; // Current size of the array
+    printf("enter size of array: ");
+    scanf("%d",&size);
+    for(int i=0;i<size;i++){
+        printf("enter your array inputs: arr[%d] ",i);
+        scanf("%d",&arr[i]);
+    }
+    int index; // Index where you want to add the element
+    printf("enter the index: ");
+    scanf("%d",&index);
+    
+    int element; // Element you want to add
+    printf("enter element which you want to add:");
+    scanf("%d",&element);
+    addElementAtIndex(arr, size, index, element);
+    
+    return 0;
+}
